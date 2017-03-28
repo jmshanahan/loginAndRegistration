3f6c9f0963350a67d428b622b14cb839192296c3
jmshanahan <comeraghsolutions@gmail.com>
2017-03-28 16:52:38 +0100

    Completed validation

--- functions/functions.php
+++ functions/functions.php
@@ -35,4 +35,93 @@ function token_generator(){
 }
 
 
+function validation_errors($error_message){
+    $error_message = <<<DELIMITER
+   <div class="alert alert-danger alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+  <strong>Warning!</strong> $error_message
+  </div>
+DELIMITER;
 
+    echo $error_message;
+}
+
+function email_exists($email){
+    $sql = "SELECT id FROM users WHERE email = '$email'";
+    $result = query($sql);
+    if(row_count($result) == 1) {
+        return true;
+    }else{
+        return false;
+    }
+}
+function username_exists($username){
+    $sql = "SELECT id FROM users WHERE username = '$username'";
+    $result = query($sql);
+    if(row_count($result) == 1) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+
+/*********************** Validation functions ***************************/
+
+function validate_user_registration(){
+    $errors = [];
+    $min = 3;
+    $max = 20;
+
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $first_name = clean($_POST['first_name']);
+        $last_name = clean($_POST['last_name']);
+        $username = clean($_POST['username']);
+        $email = clean($_POST['email']);
+        $password = clean($_POST['password']);
+        $confirm_password = clean($_POST['confirm_password']);
+
+    }
+    if(strlen($first_name)< $min){
+        $errors[] = "Your first name cannot be less than {$min} characters";
+
+    }
+    if(strlen($first_name)> $max){
+        $errors[] = "Your first name cannot be more than {$max} characters";
+    }
+    if(strlen($last_name)< $min){
+        $errors[] = "Your last name cannot be less than {$min} characters";
+
+    }
+    if(strlen($last_name)> $max){
+        $errors[] = "Your last name cannot be more than {$max} characters";
+    }
+    if(strlen($username)< $min){
+        $errors[] = "Your username cannot be less than {$min} characters";
+
+    }
+    if(strlen($username)> $max){
+        $errors[] = "Your username cannot be more than {$max} characters";
+    }
+
+    if(username_exists($username)){
+        $errors[] = "Your Username already exists";
+    }
+    if(email_exists($email)){
+        $errors[] = "Your email already exists";
+    }
+
+    if($password !== $confirm_password){
+        $errors[] = "Your passwords fields do not match";
+    }
+
+    if(!empty($errors)){
+        foreach ($errors as $error){
+            echo validation_errors($error);
+
+        }
+    }
+
+
+}
